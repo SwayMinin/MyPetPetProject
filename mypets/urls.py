dec7beb7ccabc72fb3a69992bc05fca9c3cdcaa9
swayminin <swayminin@yandex.ru>
2023-12-13 23:43:09 +0500

    Shopping cart code

--- mypets/urls.py
+++ mypets/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/', views.cart_detail, name='cart_detail'),
 ]
